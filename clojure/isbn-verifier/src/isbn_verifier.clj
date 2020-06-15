6ce133d5af969e45510b3ff57bf6ff639d568d4b
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-15 17:54:41 -0400

    better isbn verifier solution

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -2,25 +2,12 @@
   (:require [clojure.string :as str]))
 
 
-(defn valid-length? [isbn]
-  (= (count isbn) 10))
-
-
-(defn clean-isbn [isbn]
+(defn clean [isbn]
   (str/replace isbn "-" ""))
 
 
-(def valid-clean-isbn? (comp valid-length? clean-isbn))
-
-
-(def valid-checks #{\X \1 \2\ \3 \4 \5 \6 \7 \8 \9})
-
-
-(defn valid-check? [isbn]
-  (let [check (last isbn)]
-    (if (contains? valid-checks check)
-      true
-      false)))
+(defn valid-length? [isbn]
+  (= (count (clean isbn)) 10))
 
 
 (defn parse-ints [isbn]
@@ -30,20 +17,23 @@
       (mapv #(Character/digit % 10) (seq isbn)))))
 
 
-(def parse-clean (comp parse-ints clean-isbn))
+(def parse-clean (comp parse-ints clean))
 
 
-(defn no-letters? [isbn]
-  (every? #(>= % 0) (drop-last (parse-clean isbn))))
+(defn valid-form [isbn]
+  (re-matches #"(?:\d-?){9}[\dX]" isbn))
 
 
-(defn isbn? [isbn] ;; <- arglist goes here
-  (if (and (valid-clean-isbn? isbn)
-           (valid-check? isbn)
-           (no-letters? isbn))
-    (let [a (parse-clean isbn)
-          b (reverse (range 1 11))
-          c (map * a b)
-          d (reduce + c)]
-      (= (mod d 11) 0))
-    false))
+(def one-to-ten (reverse (range 1 11)))
+
+
+(defn checksum? [nums]
+  (= 0 (mod (reduce + (map * nums one-to-ten)) 11)))
+
+
+(defn isbn? [isbn]
+(if (valid-length? isbn)
+  (if-let [isbn (valid-form isbn)]
+    (checksum? (parse-clean isbn))
+    false)
+  false))
