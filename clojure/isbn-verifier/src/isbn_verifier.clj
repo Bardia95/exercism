bccea027d0b79992d6cf6812f99f2d09c88ba383
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-15 22:12:56 -0400

    final solution

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,37 +1,22 @@
 (ns isbn-verifier
   (:require [clojure.string :as str]))
 
-
-(defn clean [isbn]
-  (str/replace isbn "-" ""))
-
-
-(defn valid-length? [isbn]
-  (= (count (clean isbn)) 10))
-
-
-(defn isbn->nums [isbn]
-  (let [isbn (clean isbn)
+(defn isbn->nums
+  [isbn]
+  (let [isbn (str/replace isbn "-" "")
         check (last isbn)]
     (if (= check \X)
       (conj (mapv #(Character/digit % 10) (drop-last isbn)) 10)
-      (mapv #(Character/digit % 10) (seq isbn)))))
+      (mapv #(Character/digit % 10) isbn))))
 
 
-(defn valid-form [isbn]
-  (re-matches #"(?:\d-?){9}[\dX]" isbn))
+(defn checksum?
+  [nums]
+  (zero? (mod (reduce + (map * nums (range 10 0 -1))) 11)))
 
 
-(def one-to-ten (reverse (range 1 11)))
-
-
-(defn checksum? [nums]
-  (= 0 (mod (reduce + (map * nums one-to-ten)) 11)))
-
-
-(defn isbn? [isbn]
-  (if (valid-length? isbn)
-    (if-let [isbn (valid-form isbn)]
-      (checksum? (isbn->nums isbn))
-      false)
+(defn isbn?
+  [s]
+  (if-let [isbn (re-matches #"(?:\d-?){9}[\dX]" s)]
+    (checksum? (isbn->nums isbn))
     false))
