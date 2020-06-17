312edabd8ae3b5c2247481970ca14594b4d58b64
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-16 21:20:12 -0400

    more elegant solution

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,22 +1,26 @@
 (ns isbn-verifier
   (:require [clojure.string :as str]))
 
-(defn isbn->nums
-  [isbn]
-  (let [isbn (str/replace isbn "-" "")
-        check (last isbn)]
-    (if (= check \X)
-      (conj (mapv #(Character/digit % 10) (drop-last isbn)) 10)
-      (mapv #(Character/digit % 10) isbn))))
+
+(defn isbn->nums [isbn]
+  (let [ch->num (fn [^Character c]
+                  (cond (= \X c) 10
+                        (Character/isDigit c) (Character/digit c 10)))]
+    (keep ch->num isbn)))
+
+
+(defn divisible-by? [d n] (zero? (mod n d)))
 
 
 (defn checksum?
   [nums]
-  (zero? (mod (reduce + (map * nums (range 10 0 -1))) 11)))
+  (->> nums
+       (map * (range 10 0 -1))
+       (reduce +)
+       (divisible-by? 11)))
 
 
 (defn isbn?
   [s]
-  (if-let [isbn (re-matches #"(?:\d-?){9}[\dX]" s)]
-    (checksum? (isbn->nums isbn))
-    false))
+  (and (some? (re-matches #"(?:\d-?){9}[\dX]" s))
+       (checksum? (isbn->nums s))))
