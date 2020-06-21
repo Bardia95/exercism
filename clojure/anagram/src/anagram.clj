d83c5192f42fe5540293f568577c1400d7a30fe3
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-20 20:22:27 -0400

    anagram solution

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,5 +1,23 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :as str]))
 
-(defn anagrams-for [word prospect-list] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def alphabet-map (zipmap (range 97 123) (repeat 26 0)))
+
+
+(defn is-anagram? [s t]
+  (if (not= (count s) (count t))
+    false
+    (loop [i 0
+           c alphabet-map]
+      (if (= i (count s))
+        (= c alphabet-map)
+        (let [d (update c (int (.charAt s i)) inc)
+              e (update d (int (.charAt t i)) dec)]
+          (recur (inc i) e))))))
+
+
+(defn anagrams-for [word candidates]
+  (let [word (str/lower-case word)]
+    (filter #(and (is-anagram? word (str/lower-case %))
+                  (not= word (str/lower-case %)))
+            candidates)))
