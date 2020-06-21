69eababe078c7ec185fc8c50d006506f81123387
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-21 17:56:15 -0400

    more elegant anagram solution

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,23 +1,11 @@
 (ns anagram
   (:require [clojure.string :as str]))
 
-(def alphabet-map (zipmap (range 97 123) (repeat 26 0)))
-
-
 (defn is-anagram? [s t]
-  (if (not= (count s) (count t))
-    false
-    (loop [i 0
-           c alphabet-map]
-      (if (= i (count s))
-        (= c alphabet-map)
-        (let [d (update c (int (.charAt s i)) inc)
-              e (update d (int (.charAt t i)) dec)]
-          (recur (inc i) e))))))
-
+  (let [s (str/lower-case s)
+        t (str/lower-case t)]
+    (and (not= s t)
+         (= (frequencies s) (frequencies t)))))
 
 (defn anagrams-for [word candidates]
-  (let [word (str/lower-case word)]
-    (filter #(and (is-anagram? word (str/lower-case %))
-                  (not= word (str/lower-case %)))
-            candidates)))
+  (filter #(is-anagram? word %) candidates))
