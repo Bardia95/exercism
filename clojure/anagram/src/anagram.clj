4068d7d30001591b1bf2912ed8f990d66e14f3e9
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-21 22:45:58 -0400

    more performant solution

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,11 +1,11 @@
 (ns anagram
   (:require [clojure.string :as str]))
 
-(defn is-anagram? [s t]
-  (let [s (str/lower-case s)
-        t (str/lower-case t)]
-    (and (not= s t)
-         (= (frequencies s) (frequencies t)))))
+(defn is-anagram? [s t freq]
+  (and (not= s t)
+       (= freq (frequencies t))))
 
 (defn anagrams-for [word candidates]
-  (filter #(is-anagram? word %) candidates))
+  (let [word (str/lower-case word)
+        freq (frequencies word)]
+    (filter #(is-anagram? word (str/lower-case %) freq) candidates)))
